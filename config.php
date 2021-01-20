38c54d30b2dbdcdd932830f061e617f7b504f84a
violett_gk <violett_gk@pop-os.localdomain>
2021-01-20 16:26:13 +0200

    delete all

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $con = mysqli_connect("localhost","root","") or die("Unable to connect");
-    mysqli_select_db($con, "samplelogindb",) or die("NO database found");
-    $config['base_url'] = 'http://localhost/yourproject/';
-?>
